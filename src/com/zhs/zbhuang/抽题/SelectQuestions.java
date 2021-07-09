2bf020f49ae4e944928a06202ede530294405098
huangzhubin <huangzhubin@able-elec.com>
2021-07-09 17:57:45 +0800

    #抽题模拟

--- src/com/zhs/zbhuang/抽题/SelectQuestions.java
+++ src/com/zhs/zbhuang/抽题/SelectQuestions.java
@@ -3,9 +3,72 @@ package com.zhs.zbhuang.抽题;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SelectQuestions {
-    private static ArrayList<Integer> store = new ArrayList<>();
+    private static ArrayList<Question> store = new ArrayList<>();
+    private static List<Question> questionList = new ArrayList<>(Arrays.asList(
+            new Question(1,"1,2",1,1),
+            new Question(2,"1,2",1,1),
+            new Question(3,"1,2",1,1),
+            new Question(4,"1,2",1,1),
+            new Question(5,"1,2",1,1),
+            new Question(6,"1,2",1,1),
+            new Question(7,"1,2",1,2),
+            new Question(8,"1,2",1,2),
+            new Question(9,"1,2",1,2),
+            new Question(10,"1,2",1,2),
+            new Question(11,"1,2",1,2),
+            new Question(12,"1,2",1,2),
+            new Question(13,"1,2",1,2),
+            new Question(14,"1,2",1,2),
+            new Question(15,"1,2",1,2),
+            new Question(16,"1,2",1,2),
+            new Question(17,"1,2",1,3),
+            new Question(18,"1,2",1,3),
+            new Question(19,"1,2",1,3),
+            new Question(20,"1,2",1,3),
+            new Question(21,"1,2",2,1),
+            new Question(22,"1,2",2,1),
+            new Question(23,"1,2",2,1),
+            new Question(24,"1,2",2,1),
+            new Question(25,"1,2",2,1),
+            new Question(26,"1,2",2,1),
+            new Question(27,"1,2",2,2),
+            new Question(28,"1,2",2,2),
+            new Question(29,"1,2",2,2),
+            new Question(30,"1,2",2,2),
+            new Question(31,"1,2",2,2),
+            new Question(32,"1,2",2,2),
+            new Question(33,"1,2",2,2),
+            new Question(34,"1,2",2,3),
+            new Question(35,"1,2",2,3),
+            new Question(36,"1,2",2,3),
+            new Question(37,"1,2",2,3),
+            new Question(38,"1,2",2,3),
+            new Question(39,"1,2",2,3),
+            new Question(40,"1,2",2,3),
+            new Question(41,"1,2",3,1),
+            new Question(42,"1,2",3,1),
+            new Question(43,"1,2",3,1),
+            new Question(44,"1,2",3,1),
+            new Question(45,"1,2",3,1),
+            new Question(46,"1,2",3,1),
+            new Question(47,"1,2",3,1),
+            new Question(48,"1,2",3,1),
+            new Question(49,"1,2",3,1),
+            new Question(50,"1,2",3,1),
+            new Question(51,"1,2",3,1),
+            new Question(52,"1,2",3,2),
+            new Question(53,"1,2",3,2),
+            new Question(54,"1,2",3,2),
+            new Question(55,"1,2",3,2),
+            new Question(56,"1,2",3,2),
+            new Question(57,"1,2",3,2),
+            new Question(58,"1,2",3,2),
+            new Question(59,"1,2",3,3),
+            new Question(60,"1,2",3,3)
+    ));
 
     /**
      * 排列
@@ -13,18 +76,18 @@ public class SelectQuestions {
      * @param k 选取的元素个数
      * @param questionList 数组
      */
-    public static void arrangement(int k,List<Integer> questionList, List<List<Integer>> resultList){
+    public static void arrangement(int k,List<Question> questionList, List<List<Question>> resultList){
         if(k == 1){
             for (int i = 0; i < questionList.size(); i++) {
                 store.add(questionList.get(i));
-                ArrayList<Integer> clone = (ArrayList<Integer>) store.clone();
+                ArrayList<Question> clone = (ArrayList<Question>) store.clone();
                 resultList.add(clone);
                 store.remove(questionList.get(i));
             }
         }else if(k > 1){
             for (int i = 0; i < questionList.size(); i++) { //按顺序挑选一个元素
                 store.add(questionList.get(i)); //添加选到的元素
-                arrangement(k - 1, removeArrayElements(questionList, store.toArray(new Integer[1])), resultList); //没有取过的元素，继续挑选
+                arrangement(k - 1, removeArrayElements(questionList, questionList.get(i)), resultList); //没有取过的元素，继续挑选
                 store.remove(questionList.get(i));
             }
         }
@@ -33,36 +96,18 @@ public class SelectQuestions {
     /**
      * 移除数组某些元素（不影响原数组）
      * @param questionList 数组
-     * @param elements 待移除的元素
+     * @param question 待移除的元素
      * @return 剩余元素组成的新数组
      */
-    public static List<Integer> removeArrayElements(List<Integer> questionList, Integer... elements){
-        List<Integer> remainList = new ArrayList<>(questionList.size());
-        for(int i=0;i<questionList.size();i++){
-            boolean find = false;
-            for(int j=0;j<elements.length;j++){
-                if(questionList.get(i) == elements[j]){
-                    find = true;
-                    break;
-                }
-            }
-            if(!find){ //没有找到的元素保留下来
-                remainList.add(questionList.get(i));
-            }
-        }
-        List<Integer> remainArray = new ArrayList<>(remainList.size());
-        for(int i=0;i<remainList.size();i++){
-            remainArray.add(i, remainList.get(i));
-        }
-        return remainArray;
+    public static List<Question> removeArrayElements(List<Question> questionList, Question question){
+        return questionList.stream().filter(q -> q.getQuestionId() != question.getQuestionId()).collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4));
-        int k = 3;
         System.out.println("组合结果：");
-        List<List<Integer>> resultList = new ArrayList<>();
-        arrangement(k, list, resultList);
+        List<List<Question>> resultList = new ArrayList<>();
+        arrangement(11, questionList, resultList);
         resultList.stream().forEach(r -> System.out.println(r));
+        System.out.println(resultList.size());
     }
 }
