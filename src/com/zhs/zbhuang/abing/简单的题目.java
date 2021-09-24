6093ecbd0d555c145bcc70bad466e4da7c28f05b
huangzhubin <huangzhubin@able-elec.com>
2021-09-24 18:03:23 +0800

    #1

---
+++ src/com/zhs/zbhuang/abing/简单的题目.java
@@ -0,0 +1,17 @@
+package com.zhs.zbhuang.abing;
+
+public class 简单的题目 {
+    static int sum(int n) {
+        boolean b = n > 0 && ((n += sum(n - 1)) > 0);
+        return n;
+    }
+
+    static int sums(int n) {
+        return (int)((Math.pow(n,2)+n)) >> 1;
+    }
+
+    public static void main(String[] args) {
+        int sum = sums(10);
+        System.out.println(sum);
+    }
+}
